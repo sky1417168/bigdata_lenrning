7eb66dcd191c9e13a4929c310921934b5dba5620
sky1417168 <sky1417168@163.com>
2022-07-27 10:19:18 +0800

    注释

--- src/scala/com/code/wordcount/StreamWordCount.scala
+++ src/scala/com/code/wordcount/StreamWordCount.scala
@@ -6,16 +6,23 @@ import org.apache.flink.streaming.api.scala._
 object StreamWordCount {
   def main(args: Array[String]): Unit = {
 
+    // 创建环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
+    // 从命令行读取参数, 格式为 --host localhost --port 7777
     val parameterTool = ParameterTool.fromArgs(args)
 
+    // host
     val host = parameterTool.get("host")
 
+    // port
     val port = parameterTool.getInt("port")
 
+    // 首先 nc -lk 7777 开启socket
+    // 从socket读取数据流
     val inputDataStream: DataStream[String] = env.socketTextStream(host, port)
 
+    // 数据转换
     val resultDataStream: DataStream[(String, Int)] = inputDataStream
       .flatMap(_.split(" "))
       .filter(_.nonEmpty)
@@ -23,8 +30,10 @@ object StreamWordCount {
       .keyBy(0)
       .sum(1)
 
+    // 打印流
     resultDataStream.print().setParallelism(1)
 
+    // 启动
     env.execute("wordcount")
 
 
