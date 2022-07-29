dac9fa12496d4b066ba2bfbbf938a2e71a25698e
sky1417168 <sky1417168@163.com>
2022-07-29 09:26:26 +0800

    log

--- src/scala/com/code/example03/Example03.scala
+++ src/scala/com/code/example03/Example03.scala
@@ -1,8 +1,24 @@
 package com.code.example03
 
+import org.apache.flink.api.scala._
+import org.apache.flink.table.api.scala.BatchTableEnvironment
+
 object Example03 {
 
   def main(args: Array[String]): Unit = {
+    val env = ExecutionEnvironment.getExecutionEnvironment
+    val csvInput = env.readCsvFile("D:\\project\\my_project\\study\\bigdata_lenrning\\src\\data\\abc.csv")
+
+    //使用table api创建逻辑表
+    val tableEnv = BatchTableEnvironment.create(env)
+    val table = tableEnv.fromDataSet(csvInput)
+    tableEnv.registerTable("test_flink",table)
+    //执行sql查询
+    val result = tableEnv.sqlQuery("select * from test_flink")
+    //输出结果
+    val doubleDataSet = tableEnv.toDataSet(result)
+    doubleDataSet.print();
+
 
   }
 
